eaac5c20e7aab4ee1b42da1ba07d57b5ead48c71
jaryP <jarypomponi@gmail.com>
2025-10-25 13:12:39 +0200

    feat: loss masking
    Added the automatic masking mechanism of loss values calculated over padded values in the sequence. This should improve the convergence of the loss.

--- loss.py
+++ loss.py
@@ -23,38 +23,23 @@ class BinaryContrastiveLoss():
 
         features = torch.nn.functional.normalize(features, p=2, dim=-1)
 
-        mask = (positive_index >= 0).squeeze(-1)
-        non_masked_values = mask.sum(-1)
+        mask = (positive_index >= 0).squeeze(-1).to(device).float()
 
         positive_features = self._get_by_indexing(features, positive_index.to(device))
         negative_features = self._get_by_indexing(features, negative_index.to(device))
 
-        # pos_dist = torch.norm(features - positive_features, p=2, dim=-1)
-        # neg_dist = torch.norm(features - negative_features, p=2, dim=-1)
-
         pos_dist = torch.nn.functional.cosine_similarity(features, positive_features, dim=-1)
         neg_dist = torch.nn.functional.cosine_similarity(features, negative_features, dim=-1)
-        # neg_dist = torch.norm(features - negative_features, p=2, dim=-1)
         distances = torch.stack((pos_dist, neg_dist), -1)
-
-        # label = torch.as_tensor([1])
-        # label[:, 0] = 1
-
-        # loss = torch.nn.functional.cross_entropy(distances, label)
 
         distribution = distances.exp()
 
         loss = pos_dist.exp() / distribution.sum(-1)
+
         loss = -torch.log(loss)
 
-        # pos_loss = torch.norm(features - positive_features, p=2, dim=-1)
-        # neg_loss = 1 / (torch.norm(features - negative_features, p=2, dim=-1) + 1e-6)
-        #
-        # # masking out padded sequences
-        # pos_loss = (pos_loss * mask).sum(-1) / non_masked_values
-        #
-        # neg_loss = (neg_loss * mask).sum(-1) / non_masked_values
-        #
-        # loss = -(pos_loss + neg_loss).log()
+        loss = loss * mask
+        # remove the effected of padded tokens on the loss
+        masked_loss = loss.sum(1) / mask.sum(-1)
 
-        return loss.mean()
+        return masked_loss.mean()
