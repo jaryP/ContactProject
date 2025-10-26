0273b55f81dfc397f23de1f3b71092667dcd32a8
jaryP <jarypomponi@gmail.com>
2025-10-26 23:07:39 +0100

    feat: masked BCE loss added
    Added Masked Binary Cross Entropy loss for standard fine-tuning approach

--- loss.py
+++ loss.py
@@ -1,9 +1,10 @@
 import torch
+from torch import nn
 
 
-class BinaryContrastiveLoss():
-    def __init__(self, average = True, detach: bool = False):
-        # TODO: implement a variable for the reduction operation (?)
+class BinaryContrastiveLoss(nn.Module):
+    def __init__(self, average=True, detach: bool = False, *args, **kwargs):
+        super().__init__(*args, **kwargs)
         self._detach = detach
         self.average = average
 
@@ -20,30 +21,58 @@ class BinaryContrastiveLoss():
 
         return out
 
-    def __call__(self, features, positive_index, negative_index):
+    def forward(self, features, positive_index, negative_index, temperature=1.0):
         device = features.device
 
         features = torch.nn.functional.normalize(features, p=2, dim=-1)
 
-        mask = (positive_index >= 0).any(-1).to(device).float()
+        positive_index = positive_index.to(device)
+        pos_mask = positive_index >= 0
+        mask = pos_mask.any(-1).to(device).float()
 
-        positive_features = self._get_by_indexing(features, positive_index.to(device))
+        positive_features = self._get_by_indexing(features, positive_index)
         negative_features = self._get_by_indexing(features, negative_index.to(device))
 
         pos_dist = torch.nn.functional.cosine_similarity(features.unsqueeze(2), positive_features, dim=-1)
+        pos_dist[~pos_mask] = -1e12
         neg_dist = torch.nn.functional.cosine_similarity(features.unsqueeze(2), negative_features, dim=-1)
-        distances = torch.cat((pos_dist, neg_dist), -1)
 
-        distribution = distances.exp()
+        distances = torch.cat((pos_dist, neg_dist), -1) / temperature
 
-        loss = pos_dist.exp() / distribution.sum(-1, keepdim=True)
+        loss = pos_dist.exp() / distances.exp().sum(-1, keepdim=True)
 
-        loss = -torch.log(loss)
-        # sum over the positive residuals
-        loss = loss.mean(-1)
+        loss = -torch.log1p(loss)
+
+        # sum over the positive residuals and mask -1 positions
+        loss = loss * pos_mask
+        loss = loss.sum(-1) / pos_mask.sum(-1)
 
         loss = loss * mask
-        # remove the effected of padded tokens on the loss
+        # remove the affected of padded tokens on the loss
+        masked_loss = loss.sum(1) / mask.sum(-1)
+
+        if self.average:
+            masked_loss = masked_loss.mean()
+        return masked_loss
+
+
+class MaskedBCE(nn.Module):
+    def __init__(self, average=True, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self._inner_loss = nn.BCEWithLogitsLoss(reduction='none')
+        self.average = average
+
+    def forward(self, logits, gt_matrix, *args, **kwargs):
+        B = len(logits)
+
+        gt_matrix = gt_matrix.to(logits.device)
+
+        loss = self._inner_loss(logits.view(B, -1),
+                                gt_matrix.view(B, -1))
+
+        mask = (gt_matrix >= 0).float().view(B, -1)
+
+        loss = loss * mask
         masked_loss = loss.sum(1) / mask.sum(-1)
 
         if self.average:
